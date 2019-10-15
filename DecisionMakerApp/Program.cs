0108f645e618995502c2b30eed2f7ba678512542
Ivoire Morrell <imorrell@ltu.edu>
2019-10-15 16:18:38 -0400

    Decision maker application

---
+++ DecisionMakerApp/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Class name: Decision Maker Application
+  Programmer: Ivoire Morrell
+  Date: October 15th, 2019
+  Description: The decison maker application takes user integer input 
+  (integer between 1 and 100) and tells the user whether or not the number is even, odd, and between
+  a specific range
+ */
+
+using System;
+
+namespace DecisionMakerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize variables that will be used to carry out program fuctionality
+            int number;
+            int min = 1;
+            int max = 100;
+            bool ok = true;
+            string name;
+            string response;
+
+            Console.WriteLine("Welcome to the decision maker app. \n");
+
+            //ask the user for their name
+            Console.WriteLine("What is your name? \n");
+
+            //enter user name
+            name = Console.ReadLine();
+
+            Console.WriteLine();
+
+            //use while loop to control if the user wants to continue running program
+            while (ok)
+            {
+                //Welcome the user to the program
+                Console.WriteLine("Please enter a number between 1 and 100 " + name);
+
+                Console.WriteLine();
+
+                //use the getIntWithinRange from the myValidation class to ensure number is between 1 and 100 
+                number = myValidation.getIntWithinRange(min, max);
+
+                response = myValidation.getResponse(number);
+
+                //print the response
+                Console.WriteLine(response);
+
+                //use getContinue method to determine if user wants to continue
+                ok = myValidation.getContinue();
+
+                Console.WriteLine();
+            }
+
+            //if you make it here to your code, the user has decided exit the program. Close program with proper ending statement
+            Console.Write("Until next time, Dueces!");
+        }
+    }
+}
---
+++ DecisionMakerApp/myValidation.cs
@@ -0,0 +1,125 @@
+﻿/*Class name: My validation
+  Programmer: Ivoire Morrell
+  Date: October 14th, 2019
+  Description: This class is used to validate user input
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionMakerApp
+{
+    class myValidation
+    {
+ 
+        public static int getIntWithinRange(int min, int max)
+        {
+            //Create variable that will return the users input if it is true
+            int numberValue = 0;
+
+            //Use boolean to control while loop
+            bool isValid = false;
+
+            //While loop
+            while (isValid == false)
+            {
+
+                //Propmt user to enter a integer
+                Console.WriteLine("Please enter a integer \n");
+
+                numberValue = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                //use if else loop to check for the number value to see if its in 
+                //correct range
+                if (numberValue < min)
+                {
+                    Console.WriteLine("Error! Number must be greater than " + min + ". \n");
+                
+                }
+                else if (numberValue > max)
+                {
+                    Console.WriteLine("Error! Number must be less than " + max + ". \n");
+                }
+                else
+                {
+                    isValid = true;
+                }
+            }
+            return numberValue;
+        }
+
+        //this method is used to determine if the user wants to continue within a loop
+        public static bool getContinue()
+        {
+            //create variables
+            String choice;
+            bool ok = true;
+            bool result = true;
+
+            //create while loop to determine if user wants to continue
+            while (ok)
+            {
+                Console.WriteLine("Continue? (y/n): \n");
+
+                //retrieve user input
+                choice = Console.ReadLine();
+
+                //determine the users input and return corresponding message
+                if (choice.Equals("y", StringComparison.OrdinalIgnoreCase))
+                {
+                    //user wants to continue. exit the while loop and return true
+                    ok = false;
+                    result = true;
+                }
+                else if (choice.Equals("n", StringComparison.OrdinalIgnoreCase))
+                {
+                    //user does not want to continue
+                    ok = false;
+                    result = false;
+                }
+                else
+                {
+                    //user did not enter y or n
+                    Console.WriteLine("Error! Please enter Y or N. Please enter correct input \n");
+
+                }
+
+            }
+
+            return result;
+        }
+
+        //the getNumber response method is used to determine whether a number is even, odd, and prints out corresponding message
+        public static string getResponse(int number)
+        {
+            //create variable to return
+            string response;
+            
+            //output the number and corresponding message based off the number
+            if ((number % 2 == 0) && (number >= 1 && number <= 25))
+            {
+                //output the number and that its less then 25
+               response = "Even and less then 25 \n";
+            }
+            else if ((number % 2 == 0) && (number >= 26 && number <= 60))
+            {
+              response = "Even \n";
+            }
+            else if ((number % 2 == 0) && (number > 60))
+            {
+                response = number.ToString() + " and even \n";
+            }
+            else
+            {
+                response = number.ToString() + " and odd \n";
+            }
+
+            return response;
+        }
+
+
+    }
+}
